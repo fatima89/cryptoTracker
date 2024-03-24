2555ff7336d4ac95faffbc0b9ab245e839f485ad
fatima89 <flopezs.89@gmail.com>
2024-03-23 22:01:11 -0500

    Clase sabado 23

---
+++ app/src/main/java/com/example/cryptotrackerapp/model/Asset.kt
@@ -0,0 +1,10 @@
+package com.example.cryptotrackerapp.model
+
+class Asset(
+    val id: String,
+    val name:String,
+    val symbol:String,
+    val price:Double,
+    val percentage: Double
+) {
+}
